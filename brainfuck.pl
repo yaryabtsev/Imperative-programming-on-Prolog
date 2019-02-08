085d2a830f8af4fd8e0c4e65bd82160e68ae8a9c
yaryabtsev <44510108+yaryabtsev@users.noreply.github.com>
2019-02-09 00:52:11 +0300

    Update brainfuck.pl

--- brainfuck.pl
+++ brainfuck.pl
@@ -1,4 +1,4 @@
-block(cycle(Comands)) --> "[", convert(Comands), "]".
+block([Comands]) --> "[", convert(Comands), "]".
 block(C) --> [C|_].
 convert([]) --> "".
 convert([I|L]) --> block(I), convert(L).
@@ -6,18 +6,19 @@ fill([0],[]):-!.
 fill(T,T):-!.
 ll([A],A):-!.
 ll([_|H],V):-ll(H,V),!.
-run(62,L-E-[H|T],[E|L]-H-T1):-fill(T1,T),!.
-run(60,[H|T]-E-L,T1-H-[E|L]):-fill(T1,T),!.
-run(43,L-E-R,L-E1-R) :- E1 is E + 1,!.
-run(45,L-E-R,L-E1-R) :- E1 is E - 1,!.
-run(44,L-_-R,L-C-R):-read(Ch), string_codes(Ch,A),ll(A,C),!.
-run(46,L-C-R,L-C-R):-string_codes(W,[C]), write(W).
-run(cycle(_),L-0-R,L-0-R) :- !.
-run(cycle(Comands),L-E-R,TapeOut) :-
+next(62,L-E-[H|T],[E|L]-H-T1):-fill(T1,T),!.
+next(60,[H|T]-E-L,T1-H-[E|L]):-fill(T1,T),!.
+next(43,L-E-R,L-E1-R) :- E1 is E + 1,!.
+next(45,L-E-R,L-E1-R) :- E1 is E - 1,!.
+next(44,L-_-R,L-C-R):-read(Ch), string_codes(Ch,A),ll(A,C),!.
+next(46,L-C-R,L-C-R):-string_codes(W,[C]), write(W).
+next([_],L-0-R,L-0-R) :- !.
+next([Comands],L-E-R,TapeOut) :-
 	run(Comands,L-E-R,TapeNew),
-	run(cycle(Comands),TapeNew,TapeOut),!.
+	next([Comands],TapeNew,TapeOut),!.
+next(_,Tape,Tape) :- !.
 run([Comand|T],TapeIn,FinTape) :-
-	run(Comand,TapeIn,TapeNew),
+	next(Comand,TapeIn,TapeNew),
 	run(T,TapeNew,FinTape),!.
 run(_,Tape,Tape) :- !.
 brainfuck(Code) :-
