5c835061034174879db7f58822bfeb3e83ad62a0
yaryabtsev <44510108+yaryabtsev@users.noreply.github.com>
2019-02-09 01:19:15 +0300

    final

--- brainfuck.pl
+++ brainfuck.pl
@@ -1,27 +1,32 @@
-block([Comands]) --> "[", convert(Comands), "]".
-block(C) --> [C|_].
+loop([Program]) --> "[", convert(Program), "]".
+loop(Command) --> [Command|_].
 convert([]) --> "".
-convert([I|L]) --> block(I), convert(L).
-fill([0],[]):-!.
-fill(T,T):-!.
-ll([A],A):-!.
-ll([_|H],V):-ll(H,V),!.
-next(62,L-E-[H|T],[E|L]-H-T1):-fill(T1,T),!.
-next(60,[H|T]-E-L,T1-H-[E|L]):-fill(T1,T),!.
-next(43,L-E-R,L-E1-R) :- E1 is E + 1,!.
-next(45,L-E-R,L-E1-R) :- E1 is E - 1,!.
-next(44,L-_-R,L-C-R):-read(Ch), string_codes(Ch,A),ll(A,C),!.
-next(46,L-C-R,L-C-R):-string_codes(W,[C]), write(W).
+convert([Command|Program]) --> loop(Command), convert(Program).
+zero([0],[]):-!.
+zero(T,T):-!.
+last([Char],Char):-!.
+last([_|Chars],Char):-last(Chars,Char),!.
+next(62,L-A-[H|R],[A|L]-H-R1):-zero(R1,R),!.	% "<"
+next(60,[H|L]-A-R,L1-H-[A|R]):-zero(L1,L),!.	% ">"
+next(43,L-A-R,L-A1-R) :- A1 is A + 1,!.		% "+"
+next(45,L-A-R,L-A1-R) :- A1 is A - 1,!.		% "-"
+next(44,L-_-R,L-A-R):-		% ","
+    read(String),
+    string_codes(String,Array),
+    last(Array,A),!.
+next(46,L-A-R,L-A-R):-		% "."
+    string_codes(Char,[A]),
+    write(Char),!.
 next([_],L-0-R,L-0-R) :- !.
-next([Comands],L-E-R,TapeOut) :-
-	run(Comands,L-E-R,TapeNew),
-	next([Comands],TapeNew,TapeOut),!.
+next([Program],L-A-R,TapeOut) :-	% loop
+	run(Program,L-A-R,TapeNew),
+	next([Program],TapeNew,TapeOut),!.
 next(_,Tape,Tape) :- !.
-run([Comand|T],TapeIn,FinTape) :-
-	next(Comand,TapeIn,TapeNew),
-	run(T,TapeNew,FinTape),!.
+run([Command|Program],TapeIn,FinTape) :-
+	next(Command,TapeIn,TapeNew),
+	run(Program,TapeNew,FinTape),!.
 run(_,Tape,Tape) :- !.
 brainfuck(Code) :-
-	string_codes(Code, Program),
-	convert(Comands, Program, []),
-	run(Comands,[0]-0-[0],_),!.
+	string_codes(Code, Simple),
+	convert(Program, Simple, []),
+	run(Program,[0]-0-[0],_),!.
